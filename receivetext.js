faf46c35ee6a4a7897362659a0a1f7dc5e950bfd
Charlie Gerard <charlie.a.gerard@gmail.com>
2022-06-06 11:53:17 -0700

    small updates

--- receivetext.js
+++ receivetext.js
@@ -13,9 +13,8 @@ const onReceive = (recvPayload, recvObj) => {
   recvObj.successes++;
   var total = recvObj.failures + recvObj.successes;
   var ratio = (recvObj.failures / total) * 100;
-  recvObj.warningbox.textContent = `You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: ${
-    recvObj.failures
-  } / ${total} (${ratio.toFixed(0)}%)`;
+  recvObj.warningbox.textContent = `You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: ${recvObj.failures
+    } / ${total} (${ratio.toFixed(0)}%)`;
 };
 
 function onReceiverCreateFail(reason, recvObj) {
@@ -28,9 +27,8 @@ const onReceiveFail = (num_fails, recvObj) => {
   recvObj.failures = num_fails;
   var total = recvObj.failures + recvObj.successes;
   var ratio = (recvObj.failures / total) * 100;
-  recvObj.warningbox.textContent = `You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: ${
-    recvObj.failures
-  } / ${total} (${ratio.toFixed(0)}%)`;
+  recvObj.warningbox.textContent = `You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: ${recvObj.failures
+    } / ${total} (${ratio.toFixed(0)}%)`;
 };
 
 const onClick = (e, recvObj) => {
